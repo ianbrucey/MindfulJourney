2505b7153e0c7a26b23f9bd8e3cb7caae5c89499
ianbrucey <ib708090@gmail.com>
2025-01-01 11:10:08 -0500

    first commit

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,10 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "@db/schema";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
